f12d7a8a99eb3d1a5c4f38035c4d82eb61dc17bc
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-09-19 09:31:23 +0200

    New file

---
+++ Card.pm
@@ -0,0 +1,101 @@
+#!/usr/bin/perl
+
+=head1 NAME
+
+Card - A card is an actor with two faces.
+
+=head1 SYNOPSIS
+
+	my $card = Card->new({
+		front => $front_actor,
+		back  => $back_actor,
+	});
+
+=head1 DESCRIPTION
+
+This sample script tries to ake a two face card.
+
+=cut
+
+package Card;
+
+use strict;
+use warnings;
+
+use Glib qw(TRUE FALSE);
+use Clutter;
+use Carp;
+
+use Glib::Object::Subclass 'Clutter::Group',
+	properties => [
+		Glib::ParamSpec->object(
+			'front',
+			'Front face actor',
+			'The actor that will be shown in the front face',
+			'Clutter::Actor',
+			[ qw(readable writable) ],
+		),
+		Glib::ParamSpec->object(
+			'back',
+			'Back face actor',
+			'The actor that will be shown in the back face',
+			'Clutter::Actor',
+			[ qw(readable writable) ],
+		),
+	];
+
+
+sub new {
+	my $class = shift;
+	my ($args) = @_;
+	croak "Usage: ", __PACKAGE__, "->new(hashref)" unless ref $args eq 'HASH';
+
+	my ($front, $back) = @$args{qw(front back)};
+	my $self = Glib::Object::new(
+		__PACKAGE__,
+		front => $front,
+		back  => $back,
+	);
+	
+	$self->add($front, $back);
+	
+	return $self;
+}
+
+
+sub show_face {
+	my $self = shift;
+	my $behaviour;
+	
+	$self->{behaviour} = rotate($self, 'cw', 0, 90, sub {
+		$self->{front}->raise_top();
+		$self->{behaviour} = rotate($self, 'cw', 90, 180);
+	});
+}
+
+
+sub show_back {
+	my $self = shift;
+	
+	$self->{behaviour} = rotate($self, 'ccw', 180, 90, sub {
+		$self->{front}->lower_bottom();
+		$self->{behaviour} = rotate($self, 'ccw', 90, 0);
+	});
+}
+
+
+sub rotate {
+	my ($actor, $direction, $start, $end, $action) = @_;
+	my $timeline = Clutter::Timeline->new(3000);
+	my $alpha = Clutter::Alpha->new($timeline, 'linear');
+	my $behaviour = Clutter::Behaviour::Rotate->new($alpha, 'y-axis', $direction, $start, $end);
+	$behaviour->set_center($actor->get_width() / 2, 0, 0);
+	$behaviour->apply($actor);
+	$timeline->start();
+	$timeline->signal_connect(completed => $action) if $action;
+
+	return $behaviour;
+}
+
+# Return a true value
+1;
