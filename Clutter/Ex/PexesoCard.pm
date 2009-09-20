0973d90a6b735c671a728f0ff728bc837e28af74
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-09-20 12:16:11 +0200

    Add a license to the code

--- Clutter/Ex/PexesoCard.pm
+++ Clutter/Ex/PexesoCard.pm
@@ -184,6 +184,36 @@ sub PAINT {
 	Clutter::Cogl->set_backface_culling_enabled($culling);
 }
 
+=head1 AUTHORS
+
+Emmanuel Rodriguez E<lt>potyl@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by Emmanuel Rodriguez.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms of:
+
+=over 4
+
+=item the GNU Lesser General Public License, version 2.1; or
+
+=item the Artistic License, version 2.0.
+
+=back
+
+This module is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+You should have received a copy of the GNU Library General Public
+License along with this module; if not, see L<http://www.gnu.org/licenses/>.
+
+For the terms of The Artistic License, see L<perlartistic>.
+
+=cut
+
 # Return a true value
 1;
 
--- pexeso.pl
+++ pexeso.pl
@@ -28,6 +28,34 @@ them. You are allowed to remember the cards positions once you have seen them,
 in fact that's the purpose of the game! You continue flipping pairs of cards
 until you have successfully matched all cards.
 
+=head1 AUTHORS
+
+Emmanuel Rodriguez E<lt>potyl@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 by Emmanuel Rodriguez.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms of:
+
+=over 4
+
+=item the GNU Lesser General Public License, version 2.1; or
+
+=item the Artistic License, version 2.0.
+
+=back
+
+This module is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+You should have received a copy of the GNU Library General Public
+License along with this module; if not, see L<http://www.gnu.org/licenses/>.
+
+For the terms of The Artistic License, see L<perlartistic>.
+
 =cut
 
 use strict;
