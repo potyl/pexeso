ea8d2ee41de0ca1e0f94df3f08e72d6cf381796a
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-09-30 22:08:14 +0200

    Show multiple bars

--- progress.pl
+++ progress.pl
@@ -29,17 +29,19 @@ sub main {
 
 	my $stage = Clutter::Stage->get_default();
 	$stage->set_size(300, 300);
-
 	my ($middle_x, $middle_y) = ($stage->get_width/2, $stage->get_height/2);
+
+	# Create a progress bar
 	my $progress = create_progess($middle_x, $middle_y);
 	$stage->add($progress);
 
-
+	# A point in the middle of the screen as a reference
 	my $middle = Clutter::Rectangle->new(Clutter::Color->new(0, 0, 0xff, 0xff));
 	$middle->set_size(2, 2);
 	$middle->set_anchor_point_from_gravity('center');
 	$middle->set_position($middle_x, $middle_y);
 	$stage->add($middle);
+
 
 	my $angle = 0;
 	$stage->signal_connect('button-release-event', sub {
@@ -57,12 +59,28 @@ sub main {
 sub create_progess {
 	my ($x, $y) = @_;
 
-	my $bar = Clutter::Rectangle->new(
-		Clutter::Color->new(0xFF, 0x00, 0x00, 0xFF)
-	);
-	$bar->set_size(10, 30);
-	$bar->set_anchor_point_from_gravity('center');
-	$bar->set_position($x, $y - ($bar->get_height/2 + 10));
+	my $gap = 8;
 
-	return $bar;
+	my @bars;
+	my $group = Clutter::Group->new();
+	my $bars = 12;
+	my $angle = 360/$bars;
+	foreach my $i (0 .. $bars - 1) {
+		my $bar = Clutter::Rectangle->new(
+			Clutter::Color->new(0xFF, 0x00, 0x00, 0xFF)
+		);
+		$bar->set_size(4, 12);
+		my $gravity = $bar->get_height/2 + $gap;
+
+		$bar->set_anchor_point_from_gravity('center');
+		$bar->set_position($x, $y - $gravity);
+
+		$bar->set_rotation('z-axis', $i * $angle, 0, $gravity, 0);
+
+		push @bars, $bar;
+		$group->add($bar);
+	}
+
+
+	return $group;
 }
