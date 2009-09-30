3e2eef8a3aa4c6272e986b938509af68dc7bb63a
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-09-30 23:11:01 +0200

    Use a module for a progress bar

--- progress.pl
+++ progress.pl
@@ -17,8 +17,11 @@ This sample script tries create a progress pacifier.
 use strict;
 use warnings;
 
+use FindBin;
+use lib "$FindBin::Bin";
+
 use Glib qw(TRUE FALSE);
-use Clutter::Ex::PexesoCard;
+use Clutter::Ex::Progress;
 use Clutter qw(-init);
 
 
@@ -32,7 +35,8 @@ sub main {
 	my ($middle_x, $middle_y) = ($stage->get_width/2, $stage->get_height/2);
 
 	# Create a progress bar
-	my $progress = create_progess($middle_x, $middle_y);
+	my $progress = Clutter::Ex::Progress->new();
+	$progress->set_position($middle_x, $middle_y);
 	$stage->add($progress);
 
 	# A point in the middle of the screen as a reference
@@ -47,40 +51,11 @@ sub main {
 	$stage->signal_connect('button-release-event', sub {
 		$angle = ($angle + 15) % 360;
 		my $y = $progress->get_height/2 + 10;
-		$progress->set_rotation('z-axis', $angle, 0, $y, 0);
+		#$progress->set_rotation('z-axis', $angle, 0, $y, 0);
+		$progress->pulse();
 	});
 
 	$stage->show_all();
 	Clutter->main();
 	return 0;
-}
-
-
-sub create_progess {
-	my ($x, $y) = @_;
-
-	my $gap = 8;
-
-	my @bars;
-	my $group = Clutter::Group->new();
-	my $bars = 12;
-	my $angle = 360/$bars;
-	foreach my $i (0 .. $bars - 1) {
-		my $bar = Clutter::Rectangle->new(
-			Clutter::Color->new(0xFF, 0x00, 0x00, 0xFF)
-		);
-		$bar->set_size(4, 12);
-		my $gravity = $bar->get_height/2 + $gap;
-
-		$bar->set_anchor_point_from_gravity('center');
-		$bar->set_position($x, $y - $gravity);
-
-		$bar->set_rotation('z-axis', $i * $angle, 0, $gravity, 0);
-
-		push @bars, $bar;
-		$group->add($bar);
-	}
-
-
-	return $group;
 }
