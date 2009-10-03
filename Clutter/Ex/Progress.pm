3f1b23668f7bc06f7a304fedb3ba3d46f8c2ef3b
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-10-03 18:51:45 +0200

    Create all animations in the same way

--- Clutter/Ex/Progress.pm
+++ Clutter/Ex/Progress.pm
@@ -29,6 +29,10 @@ use Math::Trig qw(:pi);
 
 use Glib::Object::Subclass 'Clutter::Group';
 
+# Duration of the animation
+my $TIME = 2_000;
+
+
 =head2 new
 
 Creates a new card with the two given faces. The card is placed so that the back
@@ -49,7 +53,8 @@ sub new {
 
 	my $self = Glib::Object::new($class);
 
-	$self->create_actors(0, 0);
+	$self->{actors} = 12;
+	$self->create_actors();
 
 	return $self;
 }
@@ -57,18 +62,16 @@ sub new {
 
 sub create_actors {
 	my $self = shift;
-	my ($x, $y) = @_;
+	my ($x, $y) = (0, 0);
 
 	my $gap = 20;
 
-	my @actors;
-	my $actors = 12;
-	$self->{angle_step} = 360/$actors;
+	$self->{angle_step} = 360 / $self->{actors};
 
 	my $size_step = 0.025;
 	my $size = 0.8;
 	my @rgba = (0.3, 0.3, 0.3, 0.5);
-	foreach my $i (0 .. $actors - 1) {
+	foreach my $i (0 .. $self->{actors} - 1) {
 
 		# Grow the bars and change their transparency
 		$size += $size_step;
@@ -82,53 +85,59 @@ sub create_actors {
 
 		$actor->{angle} = $i * $self->{angle_step};
 		$actor->set_rotation('z-axis', $actor->{angle}, 0, $self->{gravity}, 0);
-		push @actors, $actor;
+
 		$self->add($actor);
 	}
-
-	$self->{actors} = \@actors;
 }
 
 
 sub pulse_animation_once {
 	my $self = shift;
 
-	# Animate a single frames
-	my $timeline = Clutter::Timeline->new(2000 / @{ $self->{actors} });
-	my $alpha = Clutter::Alpha->new($timeline, 'linear');
+	return if $self->{animation};
+	$self->{once_iter} ||= 0;
 
-	my ($angle_start) = $self->get_rotation('z-axis');
-	$angle_start = clamp_degrees($angle_start);
-	my $angle_end = clamp_degrees($angle_start + $self->{angle_step});
+	my $angle = ++$self->{once_iter} * $self->{angle_step};
+	$self->{once_iter} = 0 if $self->{once_iter} == $self->{actors};
 
-	my $rotation = Clutter::Behaviour::Rotate->new($alpha, 'z-axis', 'cw', $angle_start, $angle_end);
-	$rotation->set_center(0, 0, 0);
-	$rotation->apply($self);
+	my $animation = $self->create_animation($angle);
+	my $timeline = $animation->get_alpha->get_timeline;
+	$timeline->signal_connect(completed => sub {
+		delete $self->{animation};
+	});
 
 	$timeline->start();
-	$self->{rotation} = $rotation;
+	$self->{animation} = $animation;
 }
 
 
 sub pulse_animation_start {
 	my $self = shift;
-	my $timeline = Clutter::Timeline->new(2000);
-	my $alpha = Clutter::Alpha->new($timeline, 'linear');
 
-	my $rotation = Clutter::Behaviour::Rotate->new($alpha, 'z-axis', 'cw', 0, 360);
-	$rotation->set_center(0, 0, 0);
-	$rotation->apply($self);
+	return if $self->{animation};
 
+	my $animation = $self->create_animation(360);
+	my $timeline = $animation->get_alpha->get_timeline;
 	$timeline->set_loop(TRUE);
 	$timeline->start();
-	$self->{rotation} = $rotation;
+	$self->{animation} = $animation;
 }
 
 
 sub pulse_animation_stop {
 	my $self = shift;
-	my $rotation = delete $self->{rotation};
-	$rotation->get_alpha->get_timeline->stop() if $rotation;
+
+	my $animation = $self->{animation} or return;
+	my $timeline = $animation->get_alpha->get_timeline;
+
+	# Stop the animation as soon as the loop is over
+	$timeline->set_loop(FALSE);
+	$timeline->signal_connect(completed => sub {
+		delete $self->{animation};
+		# If we want to continue with a pulse_animation_once then resume from
+		# the start.
+		delete $self->{once_iter};
+	});
 }
 
 
@@ -162,6 +171,26 @@ sub clamp_degrees {
 	return $angle;
 }
 
+
+sub create_animation {
+	my $self = shift;
+	my ($angle_end) = @_;
+
+	my ($angle_start) = $self->get_rotation('z-axis');
+	$angle_start = clamp_degrees($angle_start);
+	$angle_start = 0 if $angle_start == 360;
+
+	my $time = ($angle_end - $angle_start) * $TIME / 360;
+
+	my $timeline = Clutter::Timeline->new($time);
+	my $alpha = Clutter::Alpha->new($timeline, 'linear');
+
+	my $rotation = Clutter::Behaviour::Rotate->new($alpha, 'z-axis', 'cw', $angle_start, $angle_end);
+	$rotation->set_center(0, 0, 0);
+	$rotation->apply($self);
+
+	return $rotation;
+}
 
 # A true value
 1;
